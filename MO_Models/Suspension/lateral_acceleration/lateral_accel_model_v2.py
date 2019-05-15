9683904bff7213023e5068ef8836a81c9840dae7
Unknown <martin.kawczynski@ecl18.ec-lyon.fr>
2019-05-15 15:39:40 +0200

    maj lateral accel

--- MO_Models/Suspension/lateral_acceleration/lateral_accel_model_v2.py
+++ MO_Models/Suspension/lateral_acceleration/lateral_accel_model_v2.py
@@ -13,31 +13,52 @@ from math import pi, sqrt
 
 R_skidpad=15.25/2   # rayon du skid pad en m
 g=9.81  # accélération de la pesanteur en m/s²
-m_pilot=95 # masse du pilot en kg
+rho = 1.18415 # masse volumique de l'air en kg/m³
+m_pilot=75 # masse du pilot en kg
 h_pilot=0.4 # hauteur de centre de gravité du pilote en m
 
 #%% CAR DATAS
 
-tf=1.3     # voie avant (en m)
-tr=1.15     # voie arrière (en m)
+m= 185 + 15
+h= 0.4
+
+tf= 1.24     # voie avant (en m)
+tr= 1.165     # voie arrière (en m)
 
 w=1.55    # empattement (en m)
 xf = 0.5        # répartition du poids sur l'avant
 xr = 1 - xf     # répartition du poids sur l'arrière
 
-Df=0   # déportance sur l'avant (en N)
-Dr=0    # déportance sur l'arrière (en N)
+## Aero
+
+S = 1.14 #surface effective pour la déportance en m²
+Cy = 2.13 #coefficient de portance
+
 
 #%% TIRE DATAS
 
 # Donnees extraites des modeles de GTE
 
-# Pneus 13"
-FZ=np.array([50,100,150,250,350])*g*0.453592 #0.453592 = conversion lbs en kg
-FY=np.array([647.54,1296.98,1918.52,2904.13,3756.80])*0.453592
+# Le coefficient 2/3 est un coeff donné par les testeurs de pneus: ils estiment
+# qu'une vraie piste adhère 2/3 fois moins que leur banc d'essai
+
+FZ=np.array([0,667.233,444.822,1112.055,222.411,1556.877])
+
+# Pneus 10" pour un slip angle de -1.6°
+FY=np.array([0,620.662,460.466,876.499,274.269,765.804])*2/3
+
+#Pneus 10" pour un slip angle optimal
+FY=np.array([0,1519.182,1073.523,2320.305,583.316,1923.809])*2/3
+
+# Pneus 13" pour un slip angle de -1.6°
+FY=np.array([0,923.995,714.693,1111.856,420.092,1114.576])*2/3
+
+#Pneus 13" pour un slip angle optimal
+FY=np.array([0,1783.995,1233.106,2692.902,676.343,3363.065])*2/3
 
 
-Y_poly=np.polyfit(FZ,FY,2)
+
+Y_poly=np.polyfit(FZ,FY,4)
 
 # force d'adhérence latérale du pneu en fonction de la charge
 def Y(z):
@@ -57,49 +78,17 @@ def Y(z):
 # On choisit le minimu de ces deux valeurs et on obtient l'accélération maximale telle
 # que la voiture ne dérape pas
 
-MASS=np.linspace(200,250,50)        #liste des masses
-H=np.linspace(0.05,0.6,50)          #liste des hauteurs du centre de gravité
-ACC=np.zeros([len(MASS),len(H)])
-TIME=np.zeros([len(MASS),len(H)])
+def FORCE(a):
+    Zfe=xf*m*g/2 + 1/8*Cy*rho*S*a*(R_skidpad+max(tf,tr)) + xr*m*a*h/tf
+    Zfi=xf*m*g/2 + 1/8*Cy*rho*S*a*(R_skidpad+max(tf,tr)) - xr*m*a*h/tf
+    Zre=xr*m*g/2 + 1/8*Cy*rho*S*a*(R_skidpad+max(tf,tr)) + xf*m*a*h/tr
+    Zri=xr*m*g/2 + 1/8*Cy*rho*S*a*(R_skidpad+max(tf,tr)) - xf*m*a*h/tr
+    return Y(Zfe)+Y(Zfi)+Y(Zre)+Y(Zri) - m*a
 
-Aodg=10  #une accélération en ordre de grandeur pour initier la fonction fsolve qui trouve le zéro de FORCE et TORQUE
-
-for i in range(len(MASS)):
-    for j in range(len(H)):
-        
-        m=MASS[i]+m_pilot
-        h=(MASS[i]/m)*H[j]+(m_pilot/m)*h_pilot
-        
-        
-#        def FORCE(a):
-#            Zfe=xf*m*g/2 + Df/2 + xr*m*a*h/tf
-#            Zfi=xf*m*g/2 + Df/2 - xr*m*a*h/tf
-#            Zre=xr*m*g/2 + Dr/2 + xf*m*a*h/tr
-#            Zri=xr*m*g/2 + Dr/2 - xf*m*a*h/tr
-#            return Y(Zfe)+Y(Zfi)+Y(Zre)+Y(Zri) - m*a
-        def TORQUE(a):
-            Zfe=xf*m*g/2 + Df/2 + xr*m*a*h/(2*tf)
-            Zfi=xf*m*g/2 + Df/2 - xr*m*a*h/(2*tf)
-            Zre=xr*m*g/2 + Dr/2 + xf*m*a*h/(2*tr)
-            Zri=xr*m*g/2 + Dr/2 - xf*m*a*h/(2*tr)
-            return xf*(Y(Zre)+Y(Zri))-xr*(Y(Zfe)+Y(Zfi))
-        
-        amax_force = 0
-        amax_torque = fsolve(TORQUE,Aodg)
-        amax = min(amax_force,amax_torque)
-        ACC[i,j] = amax/g
-        #TIME[i,j]=2*pi*sqrt((R_skidpad+max(tf,tr)/2)*g/amax)
+amax = fsolve(FORCE,g)[0]
+acc = amax/g
+time=2*pi*sqrt((R_skidpad+max(tf,tr)/2)/amax)
     
-
-
-
-#%% PLOT
-
-import matplotlib.pyplot as plt
-
-plt.clf()
-cont=plt.contour(MASS, H, ACC,10)
-plt.clabel(cont)
-plt.xlabel('Masse (kg)')
-plt.ylabel('hauteur de centre de gravité (m) ')
-plt.show()
+print('accélération:',acc,'g')
+print('temps au skid pad:',time,'s')
+print('score au skid-pad:',score(time))
